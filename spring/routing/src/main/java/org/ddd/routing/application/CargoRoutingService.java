89e169a560ab526955d138fd790b77ac5d6066b0
duyng2512 <duyng2512@gmail.com>
2022-01-10 03:43:08 +0700

    Add Tracking

--- spring/routing/src/main/java/org/ddd/routing/application/CargoRoutingService.java
+++ spring/routing/src/main/java/org/ddd/routing/application/CargoRoutingService.java
@@ -5,6 +5,7 @@ import org.ddd.routing.shared.TransitPath;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -13,13 +14,16 @@ public class CargoRoutingService {
     public TransitPath findOptimalRoute(String origin, String destination, String arrivalDeadline){
         TransitPath transitPath = new TransitPath();
         List<TransitEdge> transitEdges = new ArrayList<>();
+        List<String> UnLoadCode = new ArrayList<>(Arrays.asList("HK", "NY", "SIG", "CA"));
+        List<String> LoadCode = new ArrayList<>(Arrays.asList("HN", "BK", "TW", "SZ"));
+
         for (int i=0;i<4;i++) {
             TransitEdge transitEdge = new TransitEdge();
             transitEdge.setVoyageNumber("V11");
-            transitEdge.setFromUnLoCode("CHK");
+            transitEdge.setFromUnLoCode(LoadCode.get(i));
+            transitEdge.setToUnLoCode(UnLoadCode.get(i));
             transitEdge.setFromDate(new Date());
             transitEdge.setToDate(new Date());
-            transitEdge.setToUnLoCode("NYC");
             transitEdges.add(transitEdge);
         }
         transitPath.setTransitEdges(transitEdges);
--- spring/routing/src/main/java/org/ddd/routing/interfaces/CargoRoutingController.java
+++ spring/routing/src/main/java/org/ddd/routing/interfaces/CargoRoutingController.java
@@ -4,19 +4,23 @@ import org.ddd.routing.application.CargoRoutingService;
 import org.ddd.routing.shared.TransitPath;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/cargo-route")
 public class CargoRoutingController {
-    private CargoRoutingService routingService;
-
+    private final CargoRoutingService routingService;
     public CargoRoutingController(CargoRoutingService routingService) {
         this.routingService = routingService;
     }
 
     @GetMapping("/optimal")
     @ResponseBody
-    public TransitPath
+    public TransitPath findOptimalRoute( @PathVariable(value = "origin", required = false) String originUnLoCode,
+                                         @PathVariable(value = "destination", required = false) String destinationUnLoCode,
+                                         @PathVariable(value = "deadline", required = false) String deadline){
+        return routingService.findOptimalRoute(originUnLoCode, destinationUnLoCode, deadline);
+    }
 }
